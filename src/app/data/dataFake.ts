8405c7503667471a4b580353a7f62ec0ab22fe34
Arthur Guedes <arthurhenrique96120@gmail.com>
2025-03-26 20:20:24 -0300

    Primeiras Mudanças

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,15 +1,29 @@
 export const dataFake = [
   {
     "id": "1",
-    "title": "Novo filme da liga anunciado",
-    "description": "lalalalalalal",
-    "photoCover": "https://s3.amazonaws.com/blog.dentrodahistoria.com.br/wp-content/uploads/2021/06/01095905/imagem-post-herois-horizontal.png"
+    "title": "Fiat anuncia novo Argo 2026 sem mudanças na mecânica e com mudanças sutís no visual!",
+    "description": "Após diversos flagas, ​A Fiat apresentou recentemente a linha 2026 do Argo, trazendo atualizações significativas em design, tecnologia e equipamentos para manter sua competitividade no mercado de hatches compactos. As versões Trekking e Drive 1.3 AT agora contam com faróis full-LED, que aprimoram a visibilidade e conferem um visual mais moderno ao veículo. Além disso, os faróis de neblina também adotaram a tecnologia LED nessas configurações. Nas demais versões, foi incorporada uma linha de design em LED nos faróis, proporcionando um toque contemporâneo ao modelo",
+    "photoCover": "https://cdn.motor1.com/images/mgl/jl0wRN/s4/fiat-argo-drive-2026.webp"
   },
 
   {
     "id": "2",
-    "title": "Nova formaçao da liga da justica anunciada",
+    "title": "Novo visual do Cronos é visto na Argentina e mostra um novo design para a linha 2026",
     "description": "kajsndkadadls",
-    "photoCover": "https://t.ctcdn.com.br/JT9eFCopCBiNEZ0-XDjcFYeyFEU=/1200x675/smart/i632079.jpeg"
+    "photoCover": "https://www.noticiasautomotivas.com.br/ezoimgfmt/images.noticiasautomotivas.com.br/img/f/fiat-cronos-2026-flagra-3.jpg?ezimgfmt=rs:402x262/rscb1/ngcb1/notWebP"
+  },
+
+  {
+    "id": "3",
+    "title": "Novo Nissan Kicks surpreende no LATIN NCAP",
+    "description": "kajsndkadadls",
+    "photoCover": "https://cdn.motor1.com/images/mgl/YAqGy0/s3/novo-nissan-kicks-2026-e-testado-pelo-latin-ncap.webp"
+  },
+
+  {
+    "id": "4",
+    "title": "Nova Ferrari 12Cilindri desembarca no Brasil e parte dos R$ 7,9 milhões",
+    "description": "kajsndkadadls",
+    "photoCover": "https://s2-autoesporte.glbimg.com/-0sl4A_Nc_ZFS7j4jgTRLZkZqIw=/0x0:1900x1179/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2025/A/i/oOxcXOQ4yWAuGXcBL6FQ/ferrari-12-cilindri-dianteira.jpg"
   }
 ]
