12579400b44dd1608f6bacc8f009c678d141067e
Arthur Guedes <arthurhenrique96120@gmail.com>
2025-04-01 09:08:11 -0300

    feat: add new architecture impl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
-import { BigCardComponent } from './components/big-card/big-card.component';
-import { SmallCardComponent } from './components/small-card/small-card.component';
+import { MenuBarComponent } from '../presentation/components/menu-bar/menu-bar.component';
+import { BigCardComponent } from '../presentation/components/big-card/big-card.component';
+import { SmallCardComponent } from '../presentation/components/small-card/small-card.component';
 import { HomeComponent } from '../presentation/pages/home/home.component';
-import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { MenuTitleComponent } from '../presentation/components/menu-title/menu-title.component';
 import { ContentsComponent } from '../presentation/pages/contents/contents.component';
 
 @NgModule({
